5d08e9a017d491d4b0c1feaa8a394180ae4504db
Philipp <philipp@lueck-de.com>
2018-06-15 13:00:23 +0200

    Permission fix External Storage 3.0 ( Bei Georg geht es nocht nicht)

--- app/src/main/java/com/example/spohn/shopsnapapp/ProduktFotografieren.java
+++ app/src/main/java/com/example/spohn/shopsnapapp/ProduktFotografieren.java
@@ -10,14 +10,34 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProduktFotografieren extends AppCompatActivity {
 
@@ -80,5 +100,66 @@ public class ProduktFotografieren extends AppCompatActivity {
                 imageView.setImageURI(file);
             }
         }
-    }
-}
+ImageView imageView = findViewById(R.id.imageview);
+        Object image = imageView.getImageAlpha();
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg&version=2018-03-19";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("apikey", "8EGV9Gnkw4zOcL2df69ppU17GNaVTAwBncUs7KbsHtM0");
+            jsonBody.put("images_file=@image", image);
+            final String requestBody = jsonBody.toString();
+
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> params = new HashMap<String, String>();
+                    String creds = String.format("%s:%s","apikey","8EGV9Gnkw4zOcL2df69ppU17GNaVTAwBncUs7KbsHtM0");
+                    System.out.println("ok");
+                    String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.DEFAULT);
+                    params.put("Authorization", auth);
+                    return params;
+                }
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        responseString = String.valueOf(response.headers);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }}
