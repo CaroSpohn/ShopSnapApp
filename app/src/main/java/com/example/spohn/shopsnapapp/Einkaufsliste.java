05dbd7829f6ca94047d8c4e30c4b2ad675970cc9
CaroSpohn <caro.spohn@gmail.com>
2018-06-18 13:03:35 +0200

    Überarbeitung Layout

--- app/src/main/java/com/example/spohn/shopsnapapp/Einkaufsliste.java
+++ app/src/main/java/com/example/spohn/shopsnapapp/Einkaufsliste.java
@@ -1,7 +1,15 @@
 package com.example.spohn.shopsnapapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Einkaufsliste extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class Einkaufsliste extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_einkaufsliste);
+
+        ImageView mimageView = (ImageView) findViewById(R.id.imageView);
+
+        //Beginn "runde Ecken bei Imageview"
+        Bitmap mbitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.notizzettel)).getBitmap();
+        Bitmap imageRounded = Bitmap.createBitmap(mbitmap.getWidth(), mbitmap.getHeight(), mbitmap.getConfig());
+        Canvas canvas = new Canvas(imageRounded);
+        Paint mpaint = new Paint();
+        mpaint.setAntiAlias(true);
+        mpaint.setShader(new BitmapShader(mbitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        canvas.drawRoundRect((new RectF(0, 0, mbitmap.getWidth(), mbitmap.getHeight())), 100, 100, mpaint);// Round Image Corner 100 100 100 100
+        mimageView.setImageBitmap(imageRounded);
+        // Ende "runde Ecken bei ImageView"
     }
 }
